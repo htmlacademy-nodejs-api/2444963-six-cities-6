f985ab5f74e5a7f9beca1c741443e6f83cf4d447
Albet Savchenko <art.bertes@gmail.com>
2024-03-05 16:49:16 -0600

    подготовка к сдаче

--- src/shared/libs/rest/exception-filter/app.exception-filter.ts
+++ src/shared/libs/rest/exception-filter/app.exception-filter.ts
@@ -9,13 +9,16 @@ import { ApplicationError } from '../types/application-error.enum.js';
 
 @injectable()
 export class AppExceptionFilter implements ExceptionFilter {
-  constructor(
-    @inject(Component.Logger) private readonly logger: Logger
-  ) {
+  constructor(@inject(Component.Logger) private readonly logger: Logger) {
     this.logger.info('Register AppExceptionFilter');
   }
 
-  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+  public catch(
+    error: Error,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+  ): void {
     this.logger.error(error.message, error);
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
